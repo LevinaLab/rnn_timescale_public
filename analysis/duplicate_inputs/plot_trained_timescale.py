5f34328ebd3da1d5a2c451167a26882c78a29f51
schaefertim <tim@gruber-schaefer.de>
2023-08-07 16:45:28 +0200

    improve plotting

--- analysis/duplicate_inputs/plot_trained_timescale.py
+++ analysis/duplicate_inputs/plot_trained_timescale.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 
 from src.utils import load_model
 
@@ -9,8 +10,11 @@ model_path = '../../trained_models'
 
 
 def _get_mean_std_tau(
-    N_max, duplicate=1, curriculum_type='cumulative', network_number=1
+    N_max, duplicate=1, curriculum_type='cumulative', network_number=1, tau=None
 ):
+    affixes = [f'duplicate{duplicate}'] if duplicate > 1 else []
+    if tau is not None:
+        affixes.append(f'tau{float(tau)}')
     rnn = load_model(
         curriculum_type=curriculum_type,
         task='parity',
@@ -19,7 +23,7 @@ def _get_mean_std_tau(
         N_min=N_max if curriculum_type == 'single' else 2,
         device='cpu',
         base_path=model_path,
-        affixes=[f'duplicate{duplicate}'] if duplicate > 1 else [],
+        affixes=affixes,
     )
     taus = rnn.taus[0].detach().numpy()
 
@@ -27,6 +31,61 @@ def _get_mean_std_tau(
 
 
 duplicate_list = [1, 2, 3, 5, 10]
+N_list = list(range(2, 101))
+for curriculum_type in ['cumulative', 'single']:
+    fig, axs = plt.subplots(ncols=2, constrained_layout=True)
+    fig.suptitle(f'{curriculum_type}')
+    for i_duplicate, duplicate in enumerate(duplicate_list):
+        for network_number in [1, 2]:
+            for tau in [None, duplicate]:
+                tau_mean = np.zeros(len(N_list))
+                tau_std = np.zeros(len(N_list))
+                for i_N, N in enumerate(N_list):
+                    try:
+                        tau_mean[i_N], tau_std[i_N] = _get_mean_std_tau(
+                            N,
+                            duplicate=duplicate,
+                            curriculum_type=curriculum_type,
+                            network_number=network_number,
+                            tau=tau,
+                        )
+                    except FileNotFoundError:
+                        break
+                axs[0].plot(
+                    N_list[:i_N],
+                    tau_mean[:i_N],
+                    color=f'C{i_duplicate}',
+                    label=f'duplicate = {duplicate}' if (network_number == 1 and tau is None) else None,
+                    linestyle='-' if tau is None else '--',
+                )
+                axs[1].plot(
+                    N_list[:i_N],
+                    tau_std[:i_N],
+                    color=f'C{i_duplicate}',
+                    #label=f'duplicate = {duplicate}' if (network_number == 1 and tau is None) else None,
+                    linestyle='-' if tau is None else '--',
+                )
+    for ax in axs:
+        ax.set_xlabel('N')
+        ax.set_ylabel('tau')
+    axs[0].legend(frameon=False, loc='upper right')
+    axs[0].set_title('mean tau')
+    axs[1].set_title('std tau')
+
+    # Create the legend for tau
+    lines = [Line2D([0], [0], linestyle=ls, color='black') for ls in ['-', '--']]
+    fig.legend(
+        [Line2D([0], [0], linestyle=ls, color='black') for ls in ['-', '--']],
+        ['1.5', '=duplicate'],
+        title='Initial tau',
+        frameon=False,
+        loc='lower right',
+        bbox_to_anchor=(0.45, 0.5),
+    )
+
+    fig.show()
+
+"""duplicate_list = [1, 2, 3, 5, 10]
 N_list = list(range(2, 101))
 tau_means = np.zeros((len(duplicate_list), len(N_list)))
 tau_stds = np.zeros((len(duplicate_list), len(N_list)))
@@ -37,7 +96,10 @@ for curriculum_type in ['cumulative', 'single']:
         for i_N, N in enumerate(N_list):
             try:
                 tau_means[i_duplicate, i_N], tau_stds[i_duplicate, i_N] = _get_mean_std_tau(
-                    N, duplicate=duplicate, curriculum_type=curriculum_type
+                    N,
+                    duplicate=duplicate,
+                    curriculum_type=curriculum_type,
+                    tau=duplicate if duplicate > 1 else None,
                 )
             except FileNotFoundError:
                 break
@@ -49,4 +111,4 @@ for curriculum_type in ['cumulative', 'single']:
         ax.legend()
     axs[0].set_title('mean tau')
     axs[1].set_title('std tau')
-    fig.show()
+    fig.show()"""
--- analysis/duplicate_inputs/plot_training_progress.py
+++ analysis/duplicate_inputs/plot_training_progress.py
@@ -2,19 +2,39 @@ import os
 
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 
 model_path = '../../trained_models'
 
 task = 'parity'
 
 
-def _load_stats(duplicate=1, network_number=1, curriculum_type='cumulative'):
+def _load_stats(duplicate=1, network_number=1, curriculum_type='cumulative', tau=None):
     affixes = [f'duplicate{duplicate}'] if duplicate > 1 else []
+    if tau is not None:
+        affixes.append(f'tau{float(tau)}')
     affix_str = '_'
     if len(affixes) > 0:
         affix_str += '_'.join(affixes) + '_'
     rnn_subdir = f'{curriculum_type}_{task}{affix_str}network_{network_number}'
     return np.load(os.path.join(model_path, rnn_subdir, 'stats.npy'), allow_pickle=True).item()
+
+
+def _get_epoch_max_N(duplicate=1, network_number=1, curriculum_type='cumulative', tau=None):
+    stats_training: dict = _load_stats(
+        duplicate=duplicate,
+        network_number=network_number,
+        curriculum_type=curriculum_type,
+        tau=tau,
+    )
+    epochs = len(stats_training['loss'])
+    if curriculum_type == 'cumulative':
+        max_N = [len(stats_training['accuracy'][i]) for i in range(epochs)]
+    elif curriculum_type == 'single':
+        max_N = np.cumsum(np.array([acc[0] for acc in stats_training['accuracy']]) > 98) + 1
+    else:
+        raise ValueError(f'Unknown curriculum_type: {curriculum_type}')
+    return epochs, max_N
 
 
 # stats_test: dict = _load_stats(duplicate=1, network_number=1)
@@ -27,14 +47,39 @@ duplicate_list = [1, 2, 3, 5, 10]
 for curriculum_type in ['cumulative', 'single']:
     fig, ax = plt.subplots(constrained_layout=True)
     for i_duplicate, duplicate in enumerate(duplicate_list):
-        stats_training: dict = _load_stats(
-            duplicate=duplicate, network_number=1, curriculum_type=curriculum_type
-        )
-        epochs = len(stats_training['loss'])
-        max_N = [len(stats_training['accuracy'][i]) for i in range(epochs)]
-        ax.plot(range(epochs), max_N, label=f'duplicate = {duplicate}')
+        for network_number in [1, 2]:
+            for tau in [None, duplicate]:
+                try:
+                    epochs, max_N = _get_epoch_max_N(
+                        duplicate=duplicate,
+                        network_number=network_number,
+                        curriculum_type=curriculum_type,
+                        tau=tau,
+                    )
+                except FileNotFoundError:
+                    continue
+                ax.plot(
+                    range(epochs),
+                    max_N,
+                    color=f'C{i_duplicate}',
+                    label=f'duplicate = {duplicate}' if (network_number == 1 and tau is None) else None,
+                    linestyle='-' if tau is None else '--',
+                )
+
     ax.set_xlabel('epoch')
     ax.set_ylabel('max_N')
-    ax.legend()
+    ax.legend(frameon=False, loc='upper left')
+
+    # Create the legend for tau
+    lines = [Line2D([0], [0], linestyle=ls, color='black') for ls in ['-', '--']]
+    fig.legend(
+        [Line2D([0], [0], linestyle=ls, color='black') for ls in ['-', '--']],
+        ['1.5', '=duplicate'],
+        title='Initial tau',
+        frameon=False,
+        loc='lower right',
+        bbox_to_anchor=(0.95, 0.2),
+    )
+
     fig.suptitle(f'{curriculum_type} {task}')
     fig.show()
