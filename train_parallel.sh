beedf40d8f7b8cb5e2a31d1ca7912858ba81f137
Mani-Dell <manihamidi@gmail.com>
2024-03-05 13:05:00 +0100

    fix arg and increase arrays

--- train_parallel.sh
+++ train_parallel.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=general                     # stands for --partition
 #SBATCH --mem=60G
 #SBATCH --cpus-per-task=40
-#SBATCH -t 0-08:00                      # Maximum execution time (D-HH:MM)
-#SBATCH --array=0-35                     # maps 1 to N to SLURM_ARRAY_TASK_ID below
+#SBATCH -t 0-48:00                      # Maximum execution time (D-HH:MM)
+#SBATCH --array=0-100                     # maps 1 to N to SLURM_ARRAY_TASK_ID below
 #SBATCH --output=/u/mhami/rnn_timescale_public/logs/%A_%a.out # File to which STDOUT will be written - make sure this is not on $HOME
 #SBATCH --error=/u/mhami/rnn_timescale_public/logs/%A_%a.err   # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=ALL           # Type of email notification- BEGIN,END,FAIL,ALL
@@ -25,6 +25,6 @@ save_path="/u/mhami/rnn_timescale_public/trained_models"
 # takes the same arguments as train.py but instead of number of runs you need to label the run number manually with -n
 # e.g. python train.py -c cumulative -t parity -s 0 -n 0
 #python "./training/train_growth.py"
-python "./training/parallel_dispatcher.py" --path_to_script="training/train_growth.py" --params_file="prod_sweep"
+python "./training/parallel_dispatcher.py" --path_to_script="training/train_growth.py" --params_file="prod"
 
 conda deactivate
